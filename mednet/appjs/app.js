f1538f51d02c001afacedfa00d9ce81c4e3c6a83
shriyaEsha <shriya.sasank@gmail.com>
2016-11-29 20:00:04 -0800

    added table modal

--- mednet/appjs/app.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module('myApp',['ui.bootstrap','ngTable','nvd3','ngRoute','ngSanitize','myAppControllers','myAppDirectives']);
-myApp.config(['$routeProvider', function($routeProvider) {
-                $routeProvider.when("/", {templateUrl: "partials/dashboard.html"});
-                $routeProvider.when("/table", {templateUrl: "partials/table.html"});
-                $routeProvider.otherwise({redirectTo: '/'});
-                }]);
